d1026f66ab160542eb497f13cb1d4d45a3da89dc
hubo <hubo@wedocare.com>
2016-04-14 09:12:20 +0800

    use redis set insead of list

--- index.js
+++ index.js
@@ -11,18 +11,10 @@ module.exports = app;
 
 router.post('/offset/:index', function*(next) {
   let index = this.params.index;
-  redis.lpush(`offset-${index}`, this.request.body.data);
+  redis.sadd(`offset-${index}`, this.request.body.data);
   this.body = 'ok';
 });
-router.post('/offset/:index/remove', function*() {
-  let index = this.params.index;
-  let key = `offset-${index}`;
-  let data = this.request.body.data;
-  let deleteRes = yield redis.lrem(key, 1, data);
-  console.log('data to delete: ',index,data)
-  this.body = deleteRes ? 'delete ok' : 'delete fail';
 
-});
 //get random element of this index
 router.get('/offset/:index', function*(next) {
   let index = this.params.index;
@@ -32,11 +24,10 @@ router.get('/offset/:index', function*(next) {
     this.body = JSON.stringify({success: 0, error: `key ${key} not exist!`});
     return;
   }
-  let len = yield redis.llen(key);
-  let randomIndex = _.random(len - 1);
-  let data = yield redis.lrange(key, randomIndex, randomIndex);
-  this.body = JSON.stringify({success: 1, data: data[0]});
+  let data = yield redis.spop(key);
+  this.body = JSON.stringify({success: 1, data});
 });
+
 app.use(koaBody());
 app.use(router.routes());
 app.use(router.allowedMethods());
